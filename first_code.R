d0dfd42e8b671d8dd3f00069938fa1dd2e7c1a70
ErikaBellini <erika.bellini@studio.unibo.it>
2025-05-06 12:44:36 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -4,3 +4,13 @@ library(terra)
 setwd("C:/Users/LENOVO/Downloads")
 mato=rast("Matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+
+#bande
+mato
+#3 "names" perchè ha 3 livelli/bande
+#Band 1 = vicino infrarosso (estremamente riflesso dalle piante, più che il verde)
+#Band 2 = rosso
+#Band 3 = verde
+plotRGB(mato,r=2,g=3,b=1) 
+#stiamo mettendo gli infarossi al posto del blu, visualizzando l'immagine in RGB
+#tutto ciò che vedo blu è vegetazione quindi (sicuramente!), posso mettere l'infarosso in altre componenti e vedere la vegetazione di altri colori (es rosso)
